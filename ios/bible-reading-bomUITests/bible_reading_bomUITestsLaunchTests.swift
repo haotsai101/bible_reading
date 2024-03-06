6fc5143a6e3ce3da6523c207ce70f20c7162dbab
haotsai101 <tsaizhihao@gmail.com>
2024-03-05 20:41:56 -0700

    Update flavors to create 2 different apps

---
+++ ios/bible-reading-bomUITests/bible_reading_bomUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bible_reading_bomUITestsLaunchTests.swift
+//  bible-reading-bomUITests
+//
+//  Created by Zhi-Hao Tsai on 2/28/24.
+//
+
+import XCTest
+
+final class bible_reading_bomUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
