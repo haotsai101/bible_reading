08104bf29a7b8edc172cc853fe38a81a654e9df5
haotsai101 <tsaizhihao@gmail.com>
2024-03-06 22:39:26 -0700

    Sipmlify home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,177 +15,179 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
-  late Future<List<Verse>> futureVerses = Future.value([]);
-  late Future<List<Bible>> futureVersions = Future.value([]);
-  Map<String, Bible> biblesMap = {}; // Map to hold Bible ID to Bible object
-  String title = 'Bible';
+  // late Future<List<Verse>> futureVerses = Future.value([]);
+  // late Future<List<Bible>> futureVersions = Future.value([]);
+  // Map<String, Bible> biblesMap = {}; // Map to hold Bible ID to Bible object
+  // String title = 'Bible';
 
-  @override
-  initState() {
-    super.initState();
-    _fetchData();
-  }
+  // @override
+  // initState() {
+  //   super.initState();
+  //   _fetchData();
+  // }
 
-  void _fetchData() {
-    futureVerses = ReadingManager().getVersesByChapter();
-    futureVersions = ReadingManager().getBibles().then((bibles) {
-      // Initialize the map of Bible IDs to Bible objects
-      biblesMap = {for (var bible in bibles) bible.id: bible};
-      return bibles;
-    });
-    ReadingManager().getChapter().then((c) => {
-          ReadingManager().getBook().then((b) => {
-                if (b != null &&
-                    c != null &&
-                    ReadingManager().currentBibleIds.isNotEmpty)
-                  {setState(() => title = '${b.abbreviation} ${c.number}')}
-                else if (ReadingManager().currentBibleIds.isEmpty)
-                  {setState(() => title = 'Bible')}
-              })
-        });
-  }
+  // void _fetchData() {
+  //   futureVerses = ReadingManager().getVersesByChapter();
+  //   futureVersions = ReadingManager().getBibles().then((bibles) {
+  //     // Initialize the map of Bible IDs to Bible objects
+  //     biblesMap = {for (var bible in bibles) bible.id: bible};
+  //     return bibles;
+  //   });
+  //   ReadingManager().getChapter().then((c) => {
+  //         ReadingManager().getBook().then((b) => {
+  //               if (b != null &&
+  //                   c != null &&
+  //                   ReadingManager().currentBibleIds.isNotEmpty)
+  //                 {setState(() => title = '${b.abbreviation} ${c.number}')}
+  //               else if (ReadingManager().currentBibleIds.isEmpty)
+  //                 {setState(() => title = 'Bible')}
+  //             })
+  //       });
+  // }
 
-  Future<void> _handleRefresh() async {
-    setState(() {
-      futureVerses = ReadingManager().getVersesByChapter();
-      futureVersions = ReadingManager().getBibles().then((bibles) {
-        // Initialize the map of Bible IDs to Bible objects
-        biblesMap = {for (var bible in bibles) bible.id: bible};
-        return bibles;
-      });
-    });
-  }
+  // Future<void> _handleRefresh() async {
+  //   setState(() {
+  //     futureVerses = ReadingManager().getVersesByChapter();
+  //     futureVersions = ReadingManager().getBibles().then((bibles) {
+  //       // Initialize the map of Bible IDs to Bible objects
+  //       biblesMap = {for (var bible in bibles) bible.id: bible};
+  //       return bibles;
+  //     });
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        backgroundColor: Colors.blue,
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () => Scaffold.of(context).openDrawer(),
-            );
-          },
-        ),
-        actions: <Widget>[
-          FutureBuilder<List<Bible>>(
-            future: futureVersions,
-            builder:
-                (BuildContext context, AsyncSnapshot<List<Bible>> snapshot) {
-              if (snapshot.connectionState == ConnectionState.done &&
-                  snapshot.hasData) {
-                return PopupMenuButton<Bible>(
-                  onSelected: (Bible version) async {
-                    // Handle the version selection
-                    if (ReadingManager().currentBibleIds.contains(version.id)) {
-                      await ReadingManager().removeBibleId(version.id);
-                    } else {
-                      await ReadingManager().addBibleId(version.id);
-                    }
-                    setState(() {
-                      _fetchData();
-                    });
-                  },
-                  itemBuilder: (BuildContext context) {
-                    return snapshot.data!.map((Bible version) {
-                      return CheckedPopupMenuItem<Bible>(
-                        value: version,
-                        checked: ReadingManager()
-                            .currentBibleIds
-                            .contains(version.id),
-                        child: Text(version.abbreviation),
-                      );
-                    }).toList();
-                  },
-                  icon: const Icon(Icons.book),
-                );
-              } else {
-                // Display a placeholder or an empty widget if versions are not loaded yet
-                return Container();
-              }
-            },
-          ),
-          PopupMenuButton<String>(
-            onSelected: (String result) {
-              if (result == 'download') {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          DownloadScreen(updateData: _fetchData)),
-                );
-              }
-            },
-            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-              const PopupMenuItem<String>(
-                value: 'download',
-                child: Text('Download'),
-              ),
-              // Additional options can be added here
-            ],
-            icon: const Icon(Icons.more_vert),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: CustomDrawerContent(
-          updateData: _fetchData, // Pass the function directly
-        ),
-      ),
-      body: RefreshIndicator(
-        onRefresh: _handleRefresh,
-        child: GestureDetector(
-          onHorizontalDragEnd: (DragEndDetails details) async {
-            // Determine swipe direction
-            if (details.primaryVelocity! > 0) {
-              // User swiped Right - Load previous chapter
-              // Here, you need to implement logic to determine the previous chapter ID
-              await ReadingManager().goToPreviousChapter();
-              _fetchData();
-            } else if (details.primaryVelocity! < 0) {
-              // User swiped Left - Load next chapter
-              // Here, you need to implement logic to determine the next chapter ID
-              await ReadingManager().goToNextChapter();
-              _fetchData();
-            }
-          },
-          child: FutureBuilder<List<Verse>>(
-            future: futureVerses,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError ||
-                    snapshot.data == null ||
-                    snapshot.data!.isEmpty) {
-                  return const Center(
-                      child: Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text(
-                        'No Bible downloaded/selected, please download/select one first.'),
-                  ));
-                }
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    Verse verse = snapshot.data![index];
-                    return ListTile(
-                      title: Text('${verse.number} ${verse.content}'),
-                      subtitle: biblesMap.length > 1
-                          ? Text(
-                              'Bible: ${biblesMap[verse.bibleId]?.abbreviation ?? 'Unknown'}')
-                          : null, // Conditional display based on the number of items in biblesMap
-                    );
-                  },
-                );
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
-        ),
-      ),
+      // appBar: AppBar(
+      //   title: Text(title),
+      //   backgroundColor: Colors.blue,
+      //   leading: Builder(
+      //     builder: (BuildContext context) {
+      //       return IconButton(
+      //         icon: const Icon(Icons.menu),
+      //         onPressed: () => Scaffold.of(context).openDrawer(),
+      //       );
+      //     },
+      //   ),
+      //   actions: <Widget>[
+      //     FutureBuilder<List<Bible>>(
+      //       future: futureVersions,
+      //       builder:
+      //           (BuildContext context, AsyncSnapshot<List<Bible>> snapshot) {
+      //         if (snapshot.connectionState == ConnectionState.done &&
+      //             snapshot.hasData) {
+      //           return PopupMenuButton<Bible>(
+      //             onSelected: (Bible version) async {
+      //               // Handle the version selection
+      //               if (ReadingManager().currentBibleIds.contains(version.id)) {
+      //                 await ReadingManager().removeBibleId(version.id);
+      //               } else {
+      //                 await ReadingManager().addBibleId(version.id);
+      //               }
+      //               setState(() {
+      //                 _fetchData();
+      //               });
+      //             },
+      //             itemBuilder: (BuildContext context) {
+      //               return snapshot.data!.map((Bible version) {
+      //                 return CheckedPopupMenuItem<Bible>(
+      //                   value: version,
+      //                   checked: ReadingManager()
+      //                       .currentBibleIds
+      //                       .contains(version.id),
+      //                   child: Text(version.abbreviation),
+      //                 );
+      //               }).toList();
+      //             },
+      //             icon: const Icon(Icons.book),
+      //           );
+      //         } else {
+      //           // Display a placeholder or an empty widget if versions are not loaded yet
+      //           return Container();
+      //         }
+      //       },
+      //     ),
+      //     PopupMenuButton<String>(
+      //       onSelected: (String result) {
+      //         if (result == 'download') {
+      //           Navigator.push(
+      //             context,
+      //             MaterialPageRoute(
+      //                 builder: (context) =>
+      //                     DownloadScreen(updateData: _fetchData)),
+      //           );
+      //         }
+      //       },
+      //       itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+      //         const PopupMenuItem<String>(
+      //           value: 'download',
+      //           child: Text('Download'),
+      //         ),
+      //         // Additional options can be added here
+      //       ],
+      //       icon: const Icon(Icons.more_vert),
+      //     ),
+      //   ],
+      // ),
+      // drawer: Drawer(
+      //   child: CustomDrawerContent(
+      //     updateData: _fetchData, // Pass the function directly
+      //   ),
+      // ),
+      // body: RefreshIndicator(
+      //   onRefresh: _handleRefresh,
+      //   child: GestureDetector(
+      //     onHorizontalDragEnd: (DragEndDetails details) async {
+      //       // Determine swipe direction
+      //       if (details.primaryVelocity! > 0) {
+      //         // User swiped Right - Load previous chapter
+      //         // Here, you need to implement logic to determine the previous chapter ID
+      //         await ReadingManager().goToPreviousChapter();
+      //         _fetchData();
+      //       } else if (details.primaryVelocity! < 0) {
+      //         // User swiped Left - Load next chapter
+      //         // Here, you need to implement logic to determine the next chapter ID
+      //         await ReadingManager().goToNextChapter();
+      //         _fetchData();
+      //       }
+      //     },
+      //     child: FutureBuilder<List<Verse>>(
+      //       future: futureVerses,
+      //       builder: (context, snapshot) {
+      //         if (snapshot.connectionState == ConnectionState.done) {
+      //           if (snapshot.hasError ||
+      //               snapshot.data == null ||
+      //               snapshot.data!.isEmpty) {
+      //             return const Center(
+      //                 child: Padding(
+      //               padding: EdgeInsets.all(20.0),
+      //               child: Text(
+      //                   'No Bible downloaded/selected, please download/select one first.'),
+      //             ));
+      //           }
+      //           return ListView.builder(
+      //             itemCount: snapshot.data!.length,
+      //             itemBuilder: (context, index) {
+      //               Verse verse = snapshot.data![index];
+      //               return ListTile(
+      //                 title: Text('${verse.number} ${verse.content}'),
+      //                 subtitle: biblesMap.length > 1
+      //                     ? Text(
+      //                         'Bible: ${biblesMap[verse.bibleId]?.abbreviation ?? 'Unknown'}')
+      //                     : null, // Conditional display based on the number of items in biblesMap
+      //               );
+      //             },
+      //           );
+      //         } else {
+      //           return const Center(child: CircularProgressIndicator());
+      //         }
+      //       },
+      //     ),
+      //   ),
+      // ),
+      body: Text(
+          'No Bible downloaded/selected, please download/select one first.'),
     );
   }
 }
