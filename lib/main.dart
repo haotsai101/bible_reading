963ea3c3acae88811f2f8011d6628f43c69030be
haotsai101 <tsaizhihao@gmail.com>
2024-03-18 16:06:24 -0600

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-          body: Text(DatabaseHelper.getDefaultBooks().toString()),
+          body: Text(DatabaseHelper.getDefaultBooks().length.toString()),
         )
         // home: const MyHomePage(title: 'Flutter Demo Home Page'),
         );
