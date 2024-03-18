05b365c343814ad7cf197769110c038bf3dfee96
haotsai101 <tsaizhihao@gmail.com>
2024-03-17 19:44:03 -0600

    Add test

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 void main() async {
   sqfliteFfiInit();
   await dotenv.load(fileName: ".env");
-  var readingManager = ReadingManager();
-  await readingManager.initialize();
+  // var readingManager = ReadingManager();
+  // await readingManager.initialize();
 
   runApp(const MyApp());
 }
@@ -20,20 +20,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bible Reading',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Bible Reading',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const Scaffold(
+          body: Text('testing'),
+        )
+        // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        );
   }
 }
